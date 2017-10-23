7d38afa97d3d77c2ce813b867c6684041b962955
Jimmy <sigfried_sgd@hotmail.com>
2017-10-23 15:56:19 -0300

    Components Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<a className="left brand-logo">Emaily</a>
+					<ul className="right">
+						<li>Login With Google</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+export default Header;
